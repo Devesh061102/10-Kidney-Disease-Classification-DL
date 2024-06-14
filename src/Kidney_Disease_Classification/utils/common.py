dd17ae02d281dee5a7620c593a6fd2513cd95153
Devesh061102 <deveshnegi10nov@gmail.com>
2024-06-14 22:02:56 +0530

    Base Model

--- src/Kidney_Disease_Classification/utils/common.py
+++ src/Kidney_Disease_Classification/utils/common.py
@@ -8,8 +8,6 @@ from ensure import ensure_annotations
 from box import ConfigBox
 from pathlib import Path
 from typing import Any
-import base64
-
 
 
 @ensure_annotations
@@ -123,3 +121,4 @@ def get_size(path: Path) -> str:
     """
     size_in_kb = round(os.path.getsize(path)/1024)
     return f"~ {size_in_kb} KB"
+
