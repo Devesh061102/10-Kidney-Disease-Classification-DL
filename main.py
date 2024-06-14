e5d1d604e9ea9e4cf66c9737dfa9b6cd699b7760
Devesh061102 <deveshnegi10nov@gmail.com>
2024-06-14 22:21:43 +0530

    Model Training

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from Kidney_Disease_Classification import logger
 from Kidney_Disease_Classification.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from Kidney_Disease_Classification.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
-
+from Kidney_Disease_Classification.pipeline.stage_03_model_traning import ModelTrainingPipeline
 
 
 
@@ -21,6 +21,17 @@ try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
    prepare_base_model = PrepareBaseModelTrainingPipeline()
    prepare_base_model.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+STAGE_NAME = "Training"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   model_trainer = ModelTrainingPipeline()
+   model_trainer.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
         logger.exception(e)
