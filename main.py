250f4ee4f4a92d33fc7f2fb1c1840592baa3d45e
Devesh061102 <deveshnegi10nov@gmail.com>
2024-06-15 10:50:51 +0530

    APP

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from Kidney_Disease_Classification import logger
 from Kidney_Disease_Classification.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from Kidney_Disease_Classification.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
-from Kidney_Disease_Classification.pipeline.stage_03_model_traning import ModelTrainingPipeline
+from Kidney_Disease_Classification.pipeline.stage_03_model_training import ModelTrainingPipeline
 from Kidney_Disease_Classification.pipeline.stage_04_model_evaluation import EvaluationPipeline
 
 STAGE_NAME = "Data Ingestion stage"
